68e2085a12006e07df45dac10161593f4d0d6664
lichengcheng <lichengcheng@pset.suntec.net>
2018-02-27 17:15:14 +0800

    在代码设计的时候，如果派生类有自己在heap中申请的空间，而且会用到派生类向基类的转化，那么一定要声明虚析构函数。若果没有同时满足上述两个条件，则不要声明虚析构函数（申明虚析构函数和申明虚成员函数一样，都会占用一个virtual pointer）。
    Change-Id: Iaba5988761de63ad6d410948578a9c11f3a49d51

---
+++ virtualDestructDelete/virtualDestructDelete.cpp
@@ -0,0 +1,48 @@
+/*************************************************************************
+	> File Name: virtualDestructDelete.cpp
+	> Author: ma6174
+	> Mail: ma6174@163.com 
+	> Created Time: Tue 27 Feb 2018 04:55:41 PM CST
+ ************************************************************************/
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+
+class base 
+{
+    public:
+		base()
+		{
+			cout<<"this is base construct funciton \n" <<endl;
+		}
+		virtual ~base()
+		{
+			cout<<"this is base deconstruct funciton \n" <<endl;
+		}
+};
+
+class derive :public base
+{
+	public:
+		derive()
+		{
+			cout<<"this is derive construct funciton \n" <<endl;
+		}
+		~derive()
+		{
+			cout<<"this is derive deconstruct funciton \n" <<endl;
+		}
+};
+
+int main()
+{
+	base *b = new derive;
+
+	delete b;
+
+	derive b1;
+	return 0;
+}
