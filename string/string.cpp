ddffea45ef5a3f0414e963716f08f3a811a9c8bf
lichengcheng <lichengcheng@pset.suntec.net>
2018-03-02 16:36:18 +0800

    add mString class
    Change-Id: I3ab3012521557d9d21c92adfce6b7395629d3030

---
+++ string/string.cpp
@@ -0,0 +1,74 @@
+
+#include<iostream>
+#include<utility>
+#include<string.h>
+using namespace std;
+
+class mString
+{
+	public:
+		mString()
+			:data_(new char[1])
+		{
+			*data_ = 0;
+		}
+		mString(const char* str)
+			:data_(new char[strlen(str) + 1])
+		{
+			strcpy(data_, str);
+		}
+		mString(const mString& rhs)
+			:data_(new char[rhs.size() + 1])
+		{
+			strcpy(data_, rhs.c_str());
+		}
+
+		~mString()
+		{
+			delete []data_;
+		}
+
+		mString& operator=(const mString& rhs)
+		{
+			mString tmp(rhs);
+			swap(tmp);
+			return *this;
+		}
+
+		size_t size() const
+		{
+			return strlen(data_);
+		}
+		const char* c_str()const
+		{
+			return data_;
+		}
+		void swap(mString& rhs)
+		{
+			std::swap(data_, rhs.data_);
+		}
+		void print()
+		{
+			cout<<"data is "<<data_<<endl;
+		}
+
+	private:
+		char* data_;
+};
+
+
+int main()
+{
+	mString str("hello world");
+
+	mString str1= str;
+	str1.print();
+
+	mString str2(str1);
+	str2.print();
+
+	mString str3;
+
+	return 0;
+}
+
